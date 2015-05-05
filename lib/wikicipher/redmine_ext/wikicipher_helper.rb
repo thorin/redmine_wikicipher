534a92f67cf029fb0960a725bb2eed8f562fe15f
thorin <thorin.iv@gmail.com>
2015-05-05 16:13:59 +0200

    Only show the cipher toolbar button on wiki pages

--- lib/wikicipher/redmine_ext/wikicipher_helper.rb
+++ lib/wikicipher/redmine_ext/wikicipher_helper.rb
@@ -2,7 +2,7 @@ module Redmine::WikiFormatting::Textile::Helper
   def heads_for_wiki_formatter_with_wikicipher
     heads_for_wiki_formatter_without_wikicipher
 
-    unless @heads_for_wiki_formatter_with_wikicipher_included
+    unless @heads_for_wiki_formatter_with_wikicipher_included || controller_name != 'wiki'
       lang = current_language.to_s.downcase
       lang = 'en' unless %w(de en pt ru).include? lang
       content_for :header_tags do
